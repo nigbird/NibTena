fcba65e1d4a7027315f6ad865337a1e8c1942989
hos pro <hospro84@gmail.com>
2025-10-08 10:37:11 +0000

    in the search tab the suggestion list should come over the nave icons no

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -166,7 +166,7 @@ export default function Home() {
             priority
           />
         )}
-        <div className="relative z-10 p-6 space-y-8 bg-gradient-to-b from-black/60 to-transparent">
+        <div className="relative z-30 p-6 space-y-8 bg-gradient-to-b from-black/60 to-transparent">
         
           <section className="space-y-4 pt-8 pb-16 text-white text-center">
               <h1 className="text-3xl font-bold tracking-tight">

0312bbaaf7daf71e50b7ccab91ffda2c1f863ede
Nuhamin Mihretu <nuhaminmihretu9@gmail.com>
2025-11-19 08:26:52 +0000

    npm run build; npm start >>
    > nextn@0.1.0 build
    > cross-env NODE_ENV=pr

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { NextResponse, type NextRequest } from 'next/server';
 import { auth } from '../auth';
 
+export const runtime = "nodejs";
+
 export default auth((req: NextRequest) => {
   const { pathname } = req.nextUrl;
   
